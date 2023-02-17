2b93d82df51aecdb3fe7daaba4f6adb02dc2117c
Guille <guillermo.cubas.granado@gmail.com>
2023-02-17 09:50:04 +0000

    Project done

--- src/test/java/HotelScraper.java
+++ src/test/java/HotelScraper.java
@@ -1,7 +1,6 @@
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,10 +9,10 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HotelScraper {
-    private WebDriver driver;
+    private final WebDriver driver;
 
 
-    public HotelScraper(String url) {
+    public HotelScraper() {
         System.setProperty("webdriver.chrome.driver", "C:/Users/Guille/IdeaProjects/ScrapperDACD/chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=new");
@@ -23,45 +22,70 @@ public class HotelScraper {
 
     }
 
-    public void close() {
-        driver.quit();
-    }
-
-    private WebElement findElement(By locator) {
-        return new WebDriverWait(driver, 25).until(ExpectedConditions.presenceOfElementLocated(locator));
-    }
-
-    public List<String> getComments() {
+    public void getComments() {
         driver.get(HotelScraperUI.url);
-        findElement(By.xpath("//button[@class='osano-cm-accept-all osano-cm-buttons__button osano-cm-button osano-cm-button--type_accept']")).click();
-        findElement(By.cssSelector("button[class='uitk-link uitk-spacing uitk-spacing-padding-blockstart-two uitk-link-align-left uitk-link-layout-default uitk-link-medium']")).click();
-        for (int i = 0; i < 10; i++) {
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        WebElement cookiesbutton = new WebDriverWait(driver, 25)
+                .until(ExpectedConditions.elementToBeClickable(By.cssSelector(".osano-cm-accept-all.osano-cm-buttons__button.osano-cm-button.osano-cm-button--type_accept")    ));
+        executor.executeScript("arguments[0].click();", cookiesbutton);
+        WebElement commentbutton = new WebDriverWait(driver, 25)
+                .until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[class='uitk-link uitk-spacing uitk-spacing-padding-blockstart-two uitk-link-align-left uitk-link-layout-default uitk-link-medium']")    ));
+        executor.executeScript("arguments[0].click();", commentbutton);
+        while (true) {
             try {
-                findElement(By.cssSelector("button[class='uitk-button uitk-button-medium uitk-button-has-text uitk-button-secondary']")).click();
-            } catch (TimeoutException e) {
+                WebElement button =
+                        new WebDriverWait(driver, 10)
+                                .until(ExpectedConditions.presenceOfElementLocated(
+                                        By.cssSelector("button[class='uitk-button uitk-button-medium uitk-button-has-text uitk-button-secondary']")));
+                executor.executeScript("arguments[0].click();", button);
+            } catch (Exception e) {
                 break;
             }
         }
-        List<String> comments = driver.findElements(By.cssSelector("span[itemprop='description']")).stream()
-                .map(WebElement::getText)
-                .collect(Collectors.toList());
-        return comments;
+        List<WebElement> comments = driver.findElements(By.cssSelector("span[itemprop='description']"));
+        for (WebElement description : comments) {
+            System.out.println(description.getText());
+        }
     }
 
-    public String getServices() {
+    public void getServices() {
         driver.get(HotelScraperUI.url);
-        findElement(By.xpath("//button[@class='osano-cm-accept-all osano-cm-buttons__button osano-cm-button osano-cm-button--type_accept']")).click();
-        findElement(By.xpath("//div[@data-stid='hotel-amenities-list']//button[@type='button']")).click();
-        String services = findElement(By.xpath("//*[@id=\"app-layer-amenities-dialog\"]/section/div[3]"))
-                .getText();
-        return services;
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        WebElement cookiesbutton = new WebDriverWait(driver, 25)
+                .until(ExpectedConditions.elementToBeClickable(By.cssSelector(".osano-cm-accept-all.osano-cm-buttons__button.osano-cm-button.osano-cm-button--type_accept")    ));
+        executor.executeScript("arguments[0].click();", cookiesbutton);
+        WebElement servicesButton = new WebDriverWait(driver, 25)
+                .until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[data-stid='hotel-amenities-list'] button[type='button']")));
+        executor.executeScript("arguments[0].click();", servicesButton);
+        WebElement page = new WebDriverWait(driver, 25)
+                .until(ExpectedConditions.elementToBeClickable((By.cssSelector(".uitk-sheet-content.uitk-sheet-content-padded"))));
+        System.out.println(page.getText());
     }
 
-    public String getLocation(){
+    public void getLocation(){
         driver.get(HotelScraperUI.url);
-        findElement(By.xpath("//button[@class='osano-cm-accept-all osano-cm-buttons__button osano-cm-button osano-cm-button--type_accept']")).click();
-        String location = findElement(By.xpath("//div[@class='uitk-text uitk-type-300 uitk-text-default-theme uitk-layout-flex-item uitk-layout-flex-item-flex-basis-full_width']"))
-                .getText();
-        return location;
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        WebElement cookiesbutton = new WebDriverWait(driver, 25)
+                .until(ExpectedConditions.elementToBeClickable(By.cssSelector(".osano-cm-accept-all.osano-cm-buttons__button.osano-cm-button.osano-cm-button--type_accept")    ));
+        executor.executeScript("arguments[0].click();", cookiesbutton);
+        WebElement ubi = new WebDriverWait(driver,25)
+                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".uitk-text.uitk-type-300.uitk-text-default-theme.uitk-layout-flex-item.uitk-layout-flex-item-flex-basis-full_width")));
+        System.out.printf(ubi.getText());
     }
+    public void getRating(){
+        driver.get(HotelScraperUI.url);
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        WebElement cookiesbutton = new WebDriverWait(driver, 25)
+                .until(ExpectedConditions.elementToBeClickable(By.cssSelector(".osano-cm-accept-all.osano-cm-buttons__button.osano-cm-button.osano-cm-button--type_accept")    ));
+        executor.executeScript("arguments[0].click();", cookiesbutton);
+        WebElement rating = new WebDriverWait(driver,25)
+                .until(ExpectedConditions.presenceOfElementLocated(By.tagName("h3")));
+        System.out.printf(rating.getText());
+    }
+    boolean validate_url(String url) {
+        String[] parts = url.split("\\.");
+        return parts.length >= 2 && parts[1].equals("hoteles") && parts[0].equals("https://es");
+    }
+
 }
+
--- src/test/java/HotelScraperUI.java
+++ src/test/java/HotelScraperUI.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class HotelScraperUI {
@@ -9,42 +7,45 @@ public class HotelScraperUI {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the URL to scrape:");
         url = scanner.nextLine();
-
-        HotelScraper scraper = new HotelScraper(url);
+        HotelScraper scraper = new HotelScraper();
+        if (!scraper.validate_url(url)) {
+            System.out.println("The URL entered is not valid for this scraper. Please enter a URL for any hotel on es.hoteles.com.");
+            return;
+        }
 
         while (true) {
-            System.out.println("Select an operation:");
+            System.out.println("\nSelect an operation:");
             System.out.println("1. Get comments");
             System.out.println("2. Get services");
             System.out.println("3. Get location");
-            System.out.println("4. Exit");
+            System.out.println("4. Get rating");
+            System.out.println("5. Exit");
 
             int choice = scanner.nextInt();
 
             switch (choice) {
-                case 1:
-                    List<String> comments = scraper.getComments();
+                case 1 -> {
                     System.out.println("Comments:");
-                    for (String comment : comments) {
-                        System.out.println(comment);
-                    }
-                    break;
-                case 2:
-                    String services = scraper.getServices();
+                    scraper.getComments();
+                }
+                case 2 -> {
                     System.out.println("Services:");
-                    System.out.println(services);
-                    break;
-                case 3:
-                    String location = scraper.getLocation();
+                    scraper.getServices();
+                }
+                case 3 -> {
                     System.out.println("Location:");
-                    System.out.println(location);
-                    break;
-                case 4:
+                    scraper.getLocation();
+                }
+                case 4 -> {
+                    System.out.println("Rating:");
+                    scraper.getRating();
+                }
+                case 5 -> {
                     System.out.println("Exiting...");
                     scanner.close();
                     System.exit(0);
-                default:
-                    System.out.println("Invalid choice. Please enter a number from 1 to 4.");
+                }
+                default -> System.out.println("Invalid choice. Please enter a number from 1 to 5.");
             }
         }
     }
